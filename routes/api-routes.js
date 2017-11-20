94afd7eb80f0c2a789f57180a338f7ed62593eb9
Adam Haag <adam.m.h@gmail.com>
2017-11-20 17:06:41 -0500

    heroku

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,11 +2,11 @@ const nodemailer = require("nodemailer");
 
 module.exports = function(app){
   app.post('/contact', function (req, res) {
-    var user = "gaahmamhaag@gmail.com";
+    var user = "gaahmamhaag";
     var pass = "f9twIo8u";
     console.log(pass, user);
     var transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    service: 'Yahoo',
     auth: {
         user: user, // Your email id
         pass: pass // Your password
@@ -14,7 +14,7 @@ module.exports = function(app){
    });
    
    var mailOptions = {
-     from: 'gaahmamhaag@gmail.com', 
+     from: 'gaahmamhaag@yahoo.com', 
      to: 'adam.m.h@gmail.com', 
      subject: req.body.subject, 
      text: req.body.message + "\n\nReply to: " + req.body.name + " at " + req.body.email
